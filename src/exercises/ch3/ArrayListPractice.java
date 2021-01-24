222738d0c804ed31f89a8114aca241dcd053e91f
Rumple1981 <eric@koehn.net>
2021-01-23 19:28:41 -0600

    ch3 exercises completed

--- src/exercises/ch3/ArrayListPractice.java
+++ src/exercises/ch3/ArrayListPractice.java
@@ -1,28 +1,53 @@
 package exercises.ch3;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;                                             // step 4: import ArrayList utility
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+
 
 public class ArrayListPractice {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbersToSum = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        Integer theSum = ArrayListPractice.sumAllEvenValues(numbersToSum);
-        //ArrayListPractice.sumAllEvenValues(numbersToSum);
-        System.out.println("The sum is: " + theSum.toString());
+
+        ArrayPractice arrayPractice = new ArrayPractice();
+
+        String[] returnedWords = arrayPractice.splitAGivenString();
+//        List<String[]> stooges = Arrays.asList(returnedWords);
+//        ArrayList<String> wordsToSearch = stooges;
+        ArrayList<String> wordsToSearch = new ArrayList<String>(Arrays.asList(returnedWords));  // .aslist symbol not found
+
+        //Arrays.aslist("Dog", "Horse", "Cat");   // .aslist symbol not found
+       //ArrayList<String> wordsToSearch = new ArrayList<String>(Arrays.aslist("Dog", "Horse", "Cat"));  // .aslist symbol not found
+        //ArrayList<String> wordsToSearch = new ArrayList<>();
+       // wordsToSearch.put("Dog");
+
+//        ArrayList wordsToSearch=new ArrayList<>();
+//        wordsToSearch.add("Dog");
+//        wordsToSearch.add("Horse");
+//        wordsToSearch.add("Cat");
+
+        System.out.println("What word length are you looking for? ");
+        Integer searchLength = ArrayListPractice.getUserSearchInput();
+       ArrayListPractice.printWordsWithFiveLetters(wordsToSearch, searchLength);
     }
 
-    public static Integer sumAllEvenValues(ArrayList<Integer> valuesToSum) {
-        Integer sum = 0;
-        for (int i = 0; i < valuesToSum.size(); i++)  {
+    public static Integer getUserSearchInput() {
+        Scanner scanner = new Scanner(System.in);
+        Integer myUsersInt = scanner.nextInt();
+        scanner.close();
+        return myUsersInt;
+    }
 
-            if (valuesToSum.get(i) % 2 == 0) {
-                sum = sum + valuesToSum.get(i);  // or sum += valuesToSum.get(i)
-                //System.out.println(valuesToSum.get(i));
+    public static void printWordsWithFiveLetters(ArrayList<String> words, Integer searchLength) {
+        for (int i = 0; i < words.size(); i++) {
+            if (words.get(i).length() == searchLength) {
+                System.out.println(words.get(i));
             }
         }
-        return sum;
-        //return 0;
     }
 
 }
+
+
--- src/exercises/ch3/arrayPractice.java
+++ src/exercises/ch3/arrayPractice.java
@@ -22,10 +22,11 @@ public class ArrayPractice {
         }
     }
 
-    public void splitAGivenString() {                                // step 1: initialize method. goal: store individul words into an array
+    public String[] splitAGivenString() {                                // step 1: initialize method. goal: store individul words into an array
         String myProvidedString = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";  // step 2: Directly initialize the string I will split
         String[] mySplitStringS = myProvidedString.split(" ");
-        System.out.println(Arrays.toString(mySplitStringS));         // Step X: add the Java utility class Arrarys. This needs import java.util.Arrays; added on line 4 before the class ArrayPractice is started
+//        System.out.println(Arrays.toString(mySplitStringS));         // Step X: add the Java utility class Arrarys. This needs import java.util.Arrays; added on line 4 before the class ArrayPractice is started
+        return mySplitStringS;
     }
 
     public void splitAGivenStringIntoSentences() {                                // step 1: initialize method. goal: store individul words into an array
